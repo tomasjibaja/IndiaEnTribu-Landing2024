0d4ec351fc9620719bd426410d6afcfa9b5e0a2f
Tomas Jibaja <tomasjibaja@gmail.com>
2024-03-01 17:58:12 -0300

    Bastante armadito el form

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyYyY62UQlrx2mq8TXbkyciTvzuTu3_3W-xgLXzWBuihpv-hJmLiWf3hm840NWHKFMzGQ/exec';
 
 const form = document.forms['contact-form'];
+const formMsg = document.getElementById('form-msg');
+const submitBtn = document.getElementById('submit');
+const formCont = document.getElementById('form-container');
 
 form.addEventListener('submit', e => {
+    submitBtn.style.opacity = .5;
+    submitBtn.style.zIndex = -1;
+    formCont.classList.add('blinking');
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => submitOk())
@@ -11,5 +17,9 @@ form.addEventListener('submit', e => {
 })
 
 function submitOk() {
-    document.body.innerHTML += `<p>Gracias! Tus datos se cargaron correctamente.</p>`;
+    formCont.classList.remove('blinking');
+    formMsg.innerHTML = `Gracias! Tus datos se cargaron correctamente.`;
+    formMsg.style.opacity = 1;
+
+    form.reset();
 }
